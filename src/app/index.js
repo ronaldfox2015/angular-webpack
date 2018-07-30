cb26804bf75754fce167d618d06fe28a86f882aa
Your Name <you@example.com>
2018-07-30 01:01:24 -0500

    feat

--- src/app/index.js
+++ src/app/index.js
@@ -6,19 +6,21 @@ import MainCtrl from './controllers/MainCtrl'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AuthComponent from './components/auth'
 import HomeComponent from './components/home'
-
+import NavbarComponent from './components/navbar'
+// import './css/styles.css'
 const MODULE_NAME = 'SocialApp'
 angular
   .module(MODULE_NAME, [
     ngRoute,
     AuthComponent,
     HomeComponent,
+    NavbarComponent,
     angularGrid
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', { template: '<auth></auth>' })
       .when('/home', { template: '<home></home>' })
+      .when('/', { template: '<auth></auth>' })
   })
   .controller('MainCtrl', MainCtrl)
   .run()
